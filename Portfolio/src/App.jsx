b46dcd3b24f35a161fea3cec6cd1c55f6f665d8b
SaqibCod <saqibbca2021@gmail.com>
2025-03-31 16:36:46 +0530

    Navbar

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,9 +1,12 @@
 import styles from './App.module.css'
+import { Navbar } from './components/Navbar/Navbar'
 
 function App() {
   return (
     <>
-    <div className='{styles.App}'>
+    <div className={styles.App}>
+      <Navbar />
+    
     </div>
     </>
   )
